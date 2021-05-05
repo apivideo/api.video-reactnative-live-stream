aea9ffc4fa3b05107e8eb7cb2b8ad20289ed87a8
romain petit <contact.romain.petit@gmail.com>
2021-05-05 17:02:20 +0200

    add android

--- android/src/main/java/com/apivideoreactnativelivestream/ReactNativeLivestreamViewManager.kt
+++ android/src/main/java/com/apivideoreactnativelivestream/ReactNativeLivestreamViewManager.kt
@@ -1,20 +1,80 @@
 package com.apivideoreactnativelivestream
 
 import android.graphics.Color
+import android.util.Log
+import android.view.SurfaceView
 import android.view.View
+import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
+import net.ossrs.rtmp.ConnectCheckerRtmp
+import video.api.livestream_module.ApiVideoLiveStream
 
-class ReactNativeLivestreamViewManager : SimpleViewManager<View>() {
+class ReactNativeLivestreamViewManager : SimpleViewManager<SurfaceView>(), ConnectCheckerRtmp {
   override fun getName() = "ReactNativeLivestreamView"
+  private var liveStreamKey: String? = null
+  private var quality: String? = null
+  private var fps: Double? = null
+  lateinit var context: ThemedReactContext
 
-  override fun createViewInstance(reactContext: ThemedReactContext): View {
-    return View(reactContext)
+  override fun createViewInstance(reactContext: ThemedReactContext): SurfaceView {
+    context = reactContext
+    return SurfaceView(reactContext)
   }
 
-  @ReactProp(name = "color")
-  fun setColor(view: View, color: String) {
-    view.setBackgroundColor(Color.parseColor(color))
+  @ReactProp(name = "liveStreamKey")
+  fun setLiveStreamKey(newLiveStreamKey: String) {
+    if (newLiveStreamKey == liveStreamKey) return
+    liveStreamKey = newLiveStreamKey
   }
+  @ReactProp(name = "quality")
+  fun setQuality(newQuality: String) {
+    if (newQuality == quality) return
+    quality = newQuality
+  }
+  @ReactProp(name = "fps")
+  fun setFps(newFps: Double) {
+    if (newFps == fps) return
+    fps = newFps
+  }
+
+  @ReactMethod()
+  fun callItNowPlease(){
+    Log.e("Btn click", "true")
+
+    ApiVideoLiveStream(
+      ApiVideoLiveStream.Config.Builder()
+        .videoQuality(ApiVideoLiveStream.Config.Quality.QUALITY_720)
+        .videoFps(30)
+        .build()
+    )
+
+      .start(this.liveStreamKey!!, this, this.context,this)
+  }
+
+  override fun onConnectionSuccessRtmp() {
+    Log.e("connection rtmp", "success")
+  }
+
+  override fun onConnectionFailedRtmp(reason: String) {
+    Log.e("connection rtmp", "error")
+  }
+
+  override fun onNewBitrateRtmp(bitrate: Long) {
+    Log.e("new bitrate rtmp", bitrate.toString())
+  }
+
+  override fun onDisconnectRtmp() {
+    Log.e("disconnect rtmp", "success")
+  }
+
+  override fun onAuthErrorRtmp() {
+    Log.e("auth", "error")
+  }
+
+  override fun onAuthSuccessRtmp() {
+    Log.e("auth", "success")
+  }
+
 }
