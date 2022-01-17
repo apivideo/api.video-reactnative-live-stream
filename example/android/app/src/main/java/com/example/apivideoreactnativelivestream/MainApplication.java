9e55d7237b7495548d14ecdbb39b7b4dc9c2ecf6
ThibaultBee <thibault@api.video>
2022-01-17 16:30:55 +0100

    chore(build): remove jcenter as a dependency repository and update dependencies

--- example/android/app/src/main/java/com/example/apivideoreactnativelivestream/MainApplication.java
+++ example/android/app/src/main/java/com/example/apivideoreactnativelivestream/MainApplication.java
@@ -61,7 +61,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.apivideoreactnativelivestreamExample.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.example.apivideoreactnativelivestream.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
